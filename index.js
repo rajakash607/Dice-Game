bcf410282012b7cea54fefa4957eda1f4d6b30dd
rajakash607 <124488964+rajakash607@users.noreply.github.com>
2023-02-19 17:29:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var imageno = Math.round(Math.random()*6);
+var imagesouurce ="images/dice" + imageno + ".png";
+document.querySelectorAll("img")[0].setAttribute("src", imagesouurce);
+
+var imageno2 = Math.round(Math.random()*6);
+var imagesouurce2 ="images/dice" + imageno2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", imagesouurce2);
+
+if (imageno<imageno2) {
+    document.getElementsByTagName("h1")[0].innerHTML = "player2 wins";
+}
+else if (imageno==imageno2) {
+    document.getElementsByTagName("h1")[0].innerHTML = "Draw, refresh again";
+}
+else{
+    document.getElementsByTagName("h1")[0].innerHTML = "player1 wins";
+}
